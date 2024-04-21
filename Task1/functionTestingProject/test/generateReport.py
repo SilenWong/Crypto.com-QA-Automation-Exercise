cc6b4b261a19a7c1963902ddfe2fa5199b5a9eff
SilenWong <167192236+SilenWong@users.noreply.github.com>
2024-04-21 13:07:47 +0800

    Delete Task1 directory

--- Task1/functionTestingProject/test/generateReport.py
+++
@@ -1,4 +0,0 @@
-import os
-
-#put allure report(.json) of temp into report and change to .html
-os.system("allure generate ./temp -o ./reports --clean")
--- Task1/functionTestingProject/test/yaml_util.py
+++
@@ -1,8 +0,0 @@
-import os
-import yaml
-
-#function to read testcase from yaml file
-def read_yaml(yaml_name):
-    with open(os.getcwd() + '/'+yaml_name, mode='r', encoding='utf-8') as f:
-        value = yaml.load(stream=f,Loader=yaml.FullLoader)
-        return value
